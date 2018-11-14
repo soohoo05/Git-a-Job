105b271a0649cc1240a84557f4304314ae5d9b50
soohoo05 <tyler.soohoo@gmail.com>
2018-11-14 15:02:13 -0500

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
-  validates :phone, presence: true
+  validates :email, presence: true, uniqueness: true, format:{with: URI::MailTo::EMAIL_REGEXP}
+  validates :phone, presence: true, format: {with: /(\d{3}-\d{3}-\d{4}|\d{10})/}
 
 end
