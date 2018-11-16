83489b1c8cbdf7be39fe072ee48ed0c4b77bcc72
DonovanDwyer <dwyer.donovan@gmail.com>
2018-11-16 11:28:23 -0500

    changed some user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   has_many :userjoblists
   has_many :jobs, through: :userjoblists
-  validates :name, presence: true
-  validates :address, presence: true
-  validates :username, presence: true, uniqueness: true
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :address, presence: true, length: { maximum: 20 }
+  validates :username, presence: true, uniqueness: true, length: { maximum: 20 }
   validates :email, presence: true, uniqueness: true, format:{with: URI::MailTo::EMAIL_REGEXP}
   validates :phone, presence: true, format: {with: /(\d{3}-\d{3}-\d{4}|\d{10})/}
 
